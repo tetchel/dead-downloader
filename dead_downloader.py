caae7bd09b6f5f6049fbce9f1b3efa9c9a9f735f
Tim Etchells <tetchel@gmail.com>
2016-10-27 15:32:11 -0400

    made it smart with regex

--- dead_downloader.py
+++ dead_downloader.py
@@ -1,6 +1,7 @@
 import urllib.request
 import sys
 import os
+import re
 
 def get_and_save(url, folder):
 	filename = url.rsplit('/', 1)[-1]
@@ -13,7 +14,7 @@ def get_and_save(url, folder):
 		headers = response.info()
 		d = dict(response.info())
 		size_in_mb = int(d.get("Content-Length")) / 1024 / 1024
-		print(('{:.2f}' + " MB").format(size_in_mb))
+		print(('Downloading {:.2f}' + ' MB...').format(size_in_mb))
 
 	except BaseException as e:
 		print("Error:")
@@ -36,46 +37,40 @@ for i in range(discs):
 	track_counts.append(int(inp))
 
 url = input('URL of d1t01: ')
-folder = input('Folder: ')
-os.makedirs(folder,exist_ok=True)
+folder = input('Output folder: ')
+os.makedirs(folder, exist_ok=True)
 
 endofurl = url.rsplit('/', 1)[-1]
 startofurl = url.rsplit('/', 1)[0]
+disc_track_re = re.compile('d[0-9]t?[0-9][0-9]')
 
 urls = []
 for i in range(discs):
 	current = endofurl
-	veryend = current[:8]
-	
-
-	if('t' in veryend):
-		current = current[:-8] # #t##.mp3
-	else:
-		current = current[:-7] # #t##.mp3
-
-	current = current + str(i + 1)
-
-	if('t' in veryend):
-		current += 't01.mp3'
-	else:
-		current += '01.mp3'
+	#formats:
+	# d1t01
+	# d101
+	# above with string after eg d1t01-ASDF.mp3
+	match = disc_track_re.findall(current)
+	# will fail if multiple matches
+	match = match[0]
+	# disc #
+	replacement = match
+	replacement = replacement[0] + str(i+1) + replacement[2:]
 
 	for j in range(track_counts[i]):
-		copy = current
-		# chop .mp3
-		copy = copy[:-4]
-
-		copy = copy[:-2]
-
 		tracknum = j + 1
 		if(tracknum < 10):
 			tracknum = '0' + str(tracknum)
 		else:
 			tracknum = str(tracknum)
 
-		url = startofurl + '/' + copy + tracknum + '.mp3'
+		replacement = replacement[:-2] + tracknum
+
+		url = startofurl + '/' + current.replace(match, replacement)
 
 		urls.append(url)
 
 for url in urls:
 	get_and_save(url, folder)
+	#print(url)
